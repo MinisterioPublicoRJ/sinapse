f49f07ecd36cda8665383cf92571604d9fe9d73e
Arlindo Pereira <arlindo.pereira@mprj.mp.br>
2018-07-10 15:03:23 -0300

    Organizando código e incluindo preenchimento do tipo e propriedade

--- sinapse/static/js/main.js
+++ sinapse/static/js/main.js
@@ -1,4 +1,10 @@
-function init() {
+const init = () => {
+    initNeo4JD3()
+    getLabels()
+    //initSearch()
+}
+
+const initNeo4JD3 = () => {
     var neo4jd3 = new Neo4jd3('#neo4jd3', {
         highlight: [
             {
@@ -12,17 +18,12 @@ function init() {
             }
         ],
         icons: {
-//                        'Address': 'home',
             'Api': 'gear',
-//                        'BirthDate': 'birthday-cake',
             'Cookie': 'paw',
-//                        'CreditCard': 'credit-card',
-//                        'Device': 'laptop',
             'Email': 'at',
             'Git': 'git',
             'Github': 'github',
             'Google': 'google',
-//                        'icons': 'font-awesome',
             'Ip': 'map-marker',
             'Issues': 'exclamation-circle',
             'Language': 'language',
@@ -42,7 +43,6 @@ function init() {
         }, 
         images: {
             'Address': 'img/twemoji/1f3e0.svg',
-//                        'Api': 'img/twemoji/1f527.svg',
             'BirthDate': 'img/twemoji/1f382.svg',
             'Cookie': 'img/twemoji/1f36a.svg',
             'CreditCard': 'img/twemoji/1f4b3.svg',
@@ -56,45 +56,95 @@ function init() {
             'Language': 'img/twemoji/1f1f1-1f1f7.svg',
             'Options': 'img/twemoji/2699.svg',
             'Password': 'img/twemoji/1f511.svg',
-//                        'Phone': 'img/twemoji/1f4de.svg',
             'Project': 'img/twemoji/2198.svg',
             'Project|name|neo4jd3': 'img/twemoji/2196.svg',
-//                        'SecurityChallengeAnswer': 'img/twemoji/1f4ac.svg',
             'User': 'img/twemoji/1f600.svg'
-//                        'zoomFit': 'img/twemoji/2194.svg',
-//                        'zoomIn': 'img/twemoji/1f50d.svg',
-//                        'zoomOut': 'img/twemoji/1f50e.svg'
         },
         minCollision: 60,
         neo4jDataUrl: '/static/json/neo4jData.json',
         nodeRadius: 25,
         onNodeDoubleClick: function(node) {
             get('api/nextNodes?node_id=' + node.id, (data) => {
-                neo4jd3.updateWithNeo4jData(data);
+                neo4jd3.updateWithNeo4jData(data)
             });                        
         },
         onRelationshipDoubleClick: function(relationship) {
-            console.log('double click on relationship: ' + JSON.stringify(relationship));
+            console.log('double click on relationship: ' + JSON.stringify(relationship))
         },
-        //zoomFit: true
-    });
+    })
 }
 
-window.onload = init;
+const getLabels = () => {
+    get('/api/labels', setLabels)
+}
 
-function get(url, callback){
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('GET', url, true);
+const setLabels = labels => {
+    let selectLabel = document.getElementById('selectLabel')
+    selectLabel.onchange = updateProps
+    labels.sort().map(label => appendOption(selectLabel, label))
+}
+
+const appendOption = (select, optionValue) => {
+    var option = document.createElement('option')
+    option.value = optionValue
+    option.innerHTML = optionText(optionValue)
+    select.appendChild(option)
+}
+
+const updateProps = () => {
+    let label = document.getElementById('selectLabel').value
+    getNodeProperties(label)
+}
+
+const getNodeProperties = label => {
+    get(`api/nodeProperties?label=${label}`, setProps)
+}
+
+const setProps = nodeProperties => {
+    let props = nodeProperties.data[0][0]
+    console.log('props', props)
+    let selectProp = document.getElementById('selectProp')
+    props.sort().map(prop => appendOption(selectProp, prop))
+}
+
+// const initSearch = () => {
+//     let tipoBusca = document.getElementById('selectBusca').value
+//     let valorBusca = document.getElementById('textBusca').value
+//     if (!tipoBusca || !valorBusca) {
+//         return alert('É preciso escolher o tipo e preencher o valor da busca')
+//     }
+// }
+
+// const getBusca
+
+const optionText = text => {
+    switch (text) {
+        case 'veiculo':
+            return 'Veículo'
+        case 'orgao':
+            return 'Órgão'
+        case 'mgp':
+            return 'MGP'
+        default:
+            return text.substr(0, 1).toUpperCase() + text.substr(1)
+    }
+}
+
+const get = (url, callback) => {
+    var xmlhttp = new XMLHttpRequest()
+    xmlhttp.open('GET', url, true)
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4) {
             if(xmlhttp.status == 200) {
-                var obj = JSON.parse(xmlhttp.responseText);
-                console.log(obj);
+                var obj = JSON.parse(xmlhttp.responseText)
+                console.log(obj)
                 if (callback) {
                     callback(obj)
                 }
             }
         }
-    };
-    xmlhttp.send(null);
+    }
+    xmlhttp.send(null)
 }
+
+window.onload = init
