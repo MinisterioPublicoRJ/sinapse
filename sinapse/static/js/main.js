1ba6bdb6adff4896898b158b7374bad9d6c41770
Arlindo Pereira <arlindo.pereira@mprj.mp.br>
2018-07-13 17:46:38 -0300

    Removendo twemoji e usando ícones do fontawesome para nossos tipos

--- sinapse/static/js/main.js
+++ sinapse/static/js/main.js
@@ -8,67 +8,25 @@ let neo4jd3
 
 const initNeo4JD3 = () => {
     neo4jd3 = new Neo4jd3('#neo4jd3', {
-        highlight: [
-            {
-                class: 'Project',
-                property: 'name',
-                value: 'neo4jd3'
-            }, {
-                class: 'User',
-                property: 'userId',
-                value: 'eisman'
-            }
-        ],
         icons: {
-            'Api': 'gear',
-            'Cookie': 'paw',
-            'Email': 'at',
-            'Git': 'git',
-            'Github': 'github',
-            'Google': 'google',
-            'Ip': 'map-marker',
-            'Issues': 'exclamation-circle',
-            'Language': 'language',
-            'Options': 'sliders',
-            'Password': 'lock',
-            'Phone': 'phone',
-            'Project': 'folder-open',
-            'SecurityChallengeAnswer': 'commenting',
-            'pessoa': 'user',
-            'personagem':'users',
-            'veiculo': 'car',
             'empresa': 'building',
             'mgp':'balance-scale',
-            'zoomFit': 'arrows-alt',
-            'zoomIn': 'search-plus',
-            'zoomOut': 'search-minus'
+            'multa':'money',
+            'orgao':'suitcase',
+            'pessoa': 'user',
+            'personagem':'users',
+            'telefone':'phone',
+            'veiculo': 'car',
         }, 
-        images: {
-            'Address': 'img/twemoji/1f3e0.svg',
-            'BirthDate': 'img/twemoji/1f382.svg',
-            'Cookie': 'img/twemoji/1f36a.svg',
-            'CreditCard': 'img/twemoji/1f4b3.svg',
-            'Device': 'img/twemoji/1f4bb.svg',
-            'Email': 'img/twemoji/2709.svg',
-            'Git': 'img/twemoji/1f5c3.svg',
-            'Github': 'img/twemoji/1f5c4.svg',
-            'icons': 'img/twemoji/1f38f.svg',
-            'Ip': 'img/twemoji/1f4cd.svg',
-            'Issues': 'img/twemoji/1f4a9.svg',
-            'Language': 'img/twemoji/1f1f1-1f1f7.svg',
-            'Options': 'img/twemoji/2699.svg',
-            'Password': 'img/twemoji/1f511.svg',
-            'Project': 'img/twemoji/2198.svg',
-            'Project|name|neo4jd3': 'img/twemoji/2196.svg',
-            'User': 'img/twemoji/1f600.svg'
-        },
+        images: {},
+        infoPanel: false,
         minCollision: 60,
         neo4jDataUrl: '/static/json/neo4jData_vazio.json',
         nodeRadius: 25,
         onNodeDoubleClick: function(node) {
             get('api/nextNodes?node_id=' + node.id, (data) => {
                 neo4jd3.updateWithNeo4jData(data)
-            });                        
+            });
         },
         onRelationshipDoubleClick: function(relationship) {
             console.log('double click on relationship: ' + JSON.stringify(relationship))
