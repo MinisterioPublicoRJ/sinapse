b5df0a323a0d9ee0f35ceedc8377dc6579d004f6
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-09-26 16:51:19 -0300

    Busca RG processado

--- sinapse/detran/client.py
+++ sinapse/detran/client.py
@@ -19,6 +19,20 @@ xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
   </soap12:Body>
 </soap12:Envelope>"""
 
+RG_PROCESSED_BODY = """<?xml version="1.0" encoding="utf-8"?>
+<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+  <soap12:Body>
+      <BuscarProcessados xmlns="http://www.detran.rj.gov.br">
+       <CNPJ>{cnpj}</CNPJ>
+       <chave>{chave}</chave>
+       <perfil>{perfil}</perfil>
+       <IDCidadao>{idcidadao}</IDCidadao>
+      </BuscarProcessados>
+    </soap12:Body>
+</soap12:Envelope>"""
+
 
 def send_rg_query(rg):
     body = RG_BODY.format(
@@ -38,6 +52,28 @@ def send_rg_query(rg):
 
     response = requests.post(
         config('URL_CONSULTA_RG'),
+        data=body,
+        headers=headers
+    )
+
+    return response.status_code, response.content
+
+
+def get_processed_rg(rg):
+    body = RG_PROCESSED_BODY.format(
+        cnpj=config('CNPJ'),
+        chave=config('chave'),
+        perfil=config('perfil'),
+        idcidadao=rg
+    )
+
+    headers = {
+        'Content-Type': 'application/soap+xml; charset=utf-8',
+        'Content-Length': str(len(body))
+    }
+
+    response = requests.post(
+        config('URL_PROCESSADO_RG'),
         data=body,
         headers=headers
     )
--- sinapse/tests/test_detran.py
+++ sinapse/tests/test_detran.py
@@ -4,8 +4,8 @@ import responses
 
 from decouple import config
 
-from sinapse.detran.client import send_rg_query
-from sinapse.tests.fixtures import response_rg
+from sinapse.detran.client import send_rg_query, get_processed_rg
+from sinapse.tests.fixtures import response_rg, response_processado_rg
 
 
 class Photo(unittest.TestCase):
@@ -25,6 +25,23 @@ class Photo(unittest.TestCase):
         self.assertEqual(status, 200)
         self.assertEqual(content, response_rg)
         self.assertIn(b'sucesso', content)
+
+    @responses.activate
+    def test_busca_rg_processado(self):
+        responses.add(
+            responses.POST,
+            config('URL_PROCESSADO_RG'),
+            body=response_processado_rg,
+            status=200,
+            content_type='application/soap+xml; charset=utf-8'
+        )
+
+        rg = '1234'
+        status, content = get_processed_rg(rg)
+
+        self.assertEqual(status, 200)
+        self.assertEqual(content, response_processado_rg)
+        self.assertIn(b'fotoCivil', content)
 
 
 if __name__ == "__main__":
