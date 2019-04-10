2ea221d7b3c9b190445e77426f0af35ecce37392
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-04-10 15:02:44 -0300

    Utiliza node_id para busca de fotos na api interna

--- sinapse/detran/api.py
+++ sinapse/detran/api.py
@@ -7,8 +7,14 @@ from sinapse.start import login_necessario
 @app.route('/api/foto', methods=['GET'])
 @login_necessario
 def api_photo():
+    node_id = request.args.get('node_id')
     rg = request.args.get('rg')
-    photo_doc = _FOTOS_DETRAN.find_one({'rg': rg}, {'_id': 0})
+
+    if node_id is not None:
+        photo_doc = _FOTOS_DETRAN.find_one({'node_id': node_id}, {'_id': 0})
+    elif rg is not None:
+        photo_doc = _FOTOS_DETRAN.find_one({'rg': rg}, {'_id': 0})
+
     if photo_doc:
         return jsonify(photo_doc)
 
