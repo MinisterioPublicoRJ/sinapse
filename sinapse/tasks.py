c6971b317736ba688a118c3cc2b1d7a4b8ec7ec5
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-05-22 15:52:32 -0300

    Inform label to functions that search pictures

--- sinapse/tasks.py
+++ sinapse/tasks.py
@@ -5,9 +5,9 @@ from sinapse.images import get_vehicle_photo
 
 @app.task
 def get_person_photo_asynch(infos):
-    get_person_photo(infos)
+    get_person_photo(infos, label='Pessoa')
 
 
 @app.task
 def get_vehicle_photo_asynch(infos):
-    get_vehicle_photo(infos)
+    get_vehicle_photo(infos, label='Veiculo')
