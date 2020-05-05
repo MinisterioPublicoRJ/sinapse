ba67f8a69233b3b50a4264e0f2d930795b58d48a
rhenanbartels <rhenan.bartels@gmail.com>
2020-05-05 16:55:52 -0300

    Atualiza processo de busca de fotos para usar proxy

--- sinapse/detran/client.py
+++ sinapse/detran/client.py
@@ -18,27 +18,19 @@ def busca_foto(rg):
 
 
 def get_person_photo(infos, label):
-    successes = []
     for info in infos:
         if 'rg' in info:
             rg_field = 'rg'
         else:
             rg_field = 'num_rg'
-        status, content = send_rg_query(info[rg_field])
-        if b'sucesso' in content.lower()\
-                or b'foi finalizada' in content.lower():
-            successes.append(info)
 
-    for success in successes:
-        status, content = get_processed_rg(success[rg_field])
-        print('--->', status, success[rg_field])
-        photo = parse_content(content, 'fotoCivil')
-        if photo is not None and photo != '':
+        photo = busca_foto(info[rg_field])
+        if photo != '':
             _IMAGENS.update(
-                {'num_rg': success[rg_field]},
+                {'num_rg': info[rg_field]},
                 {'$set': {
                     'imagem': photo,
-                    'uuid': success['uuid'],
+                    'uuid': info['uuid'],
                     'tipo': label
                 }},
                 upsert=True
