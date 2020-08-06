01b5f64bab2a80f8366106be71aa7dfbbc0f4f46
Felipe Gomes Vieira Ferreira <felipe.gomes.ferreira@gmail.com>
2020-08-06 14:32:25 -0300

    aumento das threads do conexão

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$WORKER" ]
 then
-    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn sinapse.url:app --bind=0.0.0.0:8080 --timeout=60 --log-file - --access-logfile -
+    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn sinapse.url:app --bind=0.0.0.0:8080 --workers=12 --threads=2 --timeout=60 --log-file - --access-logfile -
 else
     celery -A sinapse.tasks worker -l info
 fi
