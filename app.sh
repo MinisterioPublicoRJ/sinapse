8ac1f11df12a1fce0366b0a0ac1a0ef64b0fffe3
rhenanbartels <rhenan.bartels@gmail.com>
2020-09-21 16:59:00 -0300

    Add kinit to app.sh
    Co-authored-by: samambaman <felipe.gomes.ferreira@gmail.com>

--- app.sh
+++ app.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+refresh_kinit() {
+    KINIT_TIMEOUT=$([[ -z "$KINIT_TIMEOUT" ]] && echo 5184000 || echo $KINIT_TIMEOUT);
+    echo "Refreshing kinit every $KINIT_TIMEOUT seconds";
+    while true; do
+        kinit mpmapas@BDA.LOCAL -kt /keys/mpmapas.keytab;
+        sleep $KINIT_TIMEOUT;
+    done
+}
+
+refresh_kinit &;
+sleep 1;
+
 if [ -z "$WORKER" ]
 then
     NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn sinapse.url:app --bind=0.0.0.0:8080 --workers=12 --threads=2 --timeout=60 --log-file - --access-logfile -
